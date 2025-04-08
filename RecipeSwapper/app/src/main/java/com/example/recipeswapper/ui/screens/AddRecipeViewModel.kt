b0e67ba336c0161fc3dd58559dc3a72e37b5a7f1
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-04-08 14:08:02 +0200

    Added recipe view model

--- RecipeSwapper/app/src/main/java/com/example/recipeswapper/ui/screens/AddRecipeViewModel.kt
+++ RecipeSwapper/app/src/main/java/com/example/recipeswapper/ui/screens/AddRecipeViewModel.kt
@@ -1,2 +1,39 @@
 package com.example.recipeswapper.ui.screens
 
+import androidx.lifecycle.ViewModel
+import com.example.recipeswapper.data.database.Recipe
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class AddRecipeState(
+    val name: String = "",
+    val description: String = ""
+) {
+    val canSubmit get() = name.isNotBlank() && description.isNotBlank()
+
+    fun toRecipe() = Recipe(
+        name = name,
+        description =  description,
+    )
+}
+
+interface AddRecipeActions {
+    fun setName(name: String)
+    fun setDescription(description: String)
+}
+
+class AddRecipeViewModel : ViewModel() {
+    private val _state = MutableStateFlow(AddRecipeState())
+    val state = _state.asStateFlow()
+
+    val actions = object : AddRecipeActions {
+        override fun setName(name: String) {
+            _state.update { it.copy(name = name) }
+        }
+
+        override fun setDescription(description: String) {
+            _state.update { it.copy(description = description) }
+        }
+    }
+}
