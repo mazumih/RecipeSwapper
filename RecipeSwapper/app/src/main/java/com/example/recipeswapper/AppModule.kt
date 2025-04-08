44cf9a11b94f06c06041a7f81b7adf79263f3670
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-04-08 14:08:31 +0200

    created modules

--- RecipeSwapper/app/src/main/java/com/example/recipeswapper/AppModule.kt
+++ RecipeSwapper/app/src/main/java/com/example/recipeswapper/AppModule.kt
@@ -1,7 +1,25 @@
 package com.example.recipeswapper
 
+import androidx.room.Room.databaseBuilder
+import com.example.recipeswapper.data.database.RecipeSwapperDatabase
+import com.example.recipeswapper.data.repositories.RecipesRepository
+import com.example.recipeswapper.ui.screens.AddRecipeViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
 
+    single { AddRecipeViewModel() }
+
+    viewModel { RecipeViewModel(get()) }
+
+    single {
+        databaseBuilder(
+            get(),
+            RecipeSwapperDatabase::class.java,
+            "recipe-list"
+        ).build()
+    }
+
+    single { RecipesRepository(get<RecipeSwapperDatabase>().recipesDAO()) }
 }
