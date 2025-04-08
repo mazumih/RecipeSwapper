7d4817971c03096f416d955f216e7a73bfdfecd5
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-04-08 14:07:11 +0200

    Added recipe view model

--- RecipeSwapper/app/src/main/java/com/example/recipeswapper/RecipeViewModel.kt
+++ RecipeSwapper/app/src/main/java/com/example/recipeswapper/RecipeViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.recipeswapper
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.recipeswapper.data.database.Recipe
+import com.example.recipeswapper.data.repositories.RecipesRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+data class RecipesState(val recipes: List<Recipe>)
+
+class RecipeViewModel(
+    private val repository: RecipesRepository
+) : ViewModel() {
+    val state = repository.recipes.map { RecipesState(recipes = it) }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = RecipesState(emptyList())
+    )
+
+    fun addRecipe(recipe: Recipe) = viewModelScope.launch {
+        repository.upsert(recipe)
+    }
+
+    fun deleteRecipe(recipe: Recipe) = viewModelScope.launch {
+        repository.delete(recipe)
+    }
+}
