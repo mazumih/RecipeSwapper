afdc160638a7fb627a6cf5475d87ff8798cebf5f
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-04-08 14:06:45 +0200

    Added repository for recipes

--- RecipeSwapper/app/src/main/java/com/example/recipeswapper/data/repositories/RecipesRepository.kt
+++ RecipeSwapper/app/src/main/java/com/example/recipeswapper/data/repositories/RecipesRepository.kt
@@ -1,4 +1,15 @@
 package com.example.recipeswapper.data.repositories
 
-class RecipesRepository {
+import com.example.recipeswapper.data.database.Recipe
+import com.example.recipeswapper.data.database.RecipesDAO
+import kotlinx.coroutines.flow.Flow
+
+class RecipesRepository(
+    private val dao: RecipesDAO
+) {
+    val recipes: Flow<List<Recipe>> = dao.getAll()
+
+    suspend fun upsert(recipe: Recipe) = dao.upsert(recipe)
+
+    suspend fun delete(recipe: Recipe) = dao.delete(recipe)
 }
